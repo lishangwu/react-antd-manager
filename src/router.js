d840978e3bbac7a2b2dc14747cb7149eefd8f3ec
Robin <544768940@qq.com>
2019-11-05 02:58:53 +0800

    test

--- src/router.js
+++ src/router.js
@@ -31,6 +31,10 @@ import OrderDetail from './pages/order/detail'
 import User from './pages/user'
 import BikeMap from './pages/map/bikeMap'
 
+import Bar from './pages/echarts/bar'
+import Line from './pages/echarts/line'
+import Pie from './pages/echarts/pie'
+
 import Common from './common'
 
 import Nomatch from './pages/nomatch'
@@ -72,6 +76,12 @@ export default class IRouter extends React.Component {
                                 <Route path="/admin/user" component={User} />
                                 {/* 车辆地图 */}
                                 <Route path="/admin/bikeMap" component={BikeMap} />
+                                {/* 柱形图 */}
+                                <Route path="/admin/charts/bar" component={Bar} />
+                                {/* 柱形图 */}
+                                <Route path="/admin/charts/line" component={Line} />
+                                {/* 柱形图 */}
+                                <Route path="/admin/charts/pie" component={Pie} />
 
                                 <Route component={Nomatch} />
 
